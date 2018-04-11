2b0884de767982bc5e591f6b0bf3cf69eb250540
Suleiman Mamman A <suleimamman@gmail.com>
2018-04-11 11:51:32 +0100

    Adding bootstrap styling and creating the log in and registration page

---
+++ view/head.php
@@ -0,0 +1,12 @@
+<head>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>webCrawler</title>
+    <link href="assets/plugins/bootstrap/bootstrap.css" rel="stylesheet" />
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link href="assets/plugins/pace/pace-theme-big-counter.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <link href="assets/css/main-style.css" rel="stylesheet" />
+    <link href="assets/css/custom.css" rel="stylesheet" />
+
+</head>
