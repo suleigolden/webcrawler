f7f2107fb15e2556cf246c8a4c3422b9c6244d7f
Suleiman Mamman A <suleimamman@gmail.com>
2018-04-13 23:12:11 +0100

    Page not found page created

---
+++ view/404notfound.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+include_once("head.php");
+?>
+<body class="">
+
+    <div class="container">
+       
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4 text-center logo-margin ">
+              <h1>Sorry, the page you are looking for could not be found.</h1>
+               <a href="?/=home" class="btn btn-primary">Already have an Account?. Log In</a>
+               <br><br><br>
+               <a href="?/=register" class="btn btn-primary">I Don't have an Account?</a>  
+              </div>
+        </div>
+    </div>
+
+<?php
+include_once("footer.php");
+?>
+
+</body>
+
+</html>
